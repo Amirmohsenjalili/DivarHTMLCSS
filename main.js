b3d7962b8d5ae1b2f28207908244f69fc0d672db
Amirmohsen Jalili <amirmohsen.jalili2016@gmail.com>
2023-07-02 15:12:42 +0330

    project The project was completed

---
+++ main.js
@@ -0,0 +1,45 @@
+function placeFunction() {
+    var Place = document.querySelector("#filterPlace");
+    Place.classList.toggle("noDisplay")
+}
+
+function priceFunction() {
+    var Price = document.querySelector("#filterPrice");
+    Price.classList.toggle("noDisplay")
+}
+
+function conditionFunction() {
+    var Condition = document.querySelector("#filterCondition");
+    Condition.classList.toggle("noDisplay")
+}
+
+// const Category = document.querySelector("#showCategory");
+// const showCategory = document.querySelector(".showCategory");
+// const Browse = document.querySelector("#browse")
+
+// Category.addEventListener("mouseenter", () => {
+//     showCategory.style.display = 'block';
+//     showCategory.style.animation = 'showUp 0.5s 1';
+//     Category.style.animation = 'showUp 0.5s 1';
+//     Browse.style.opacity = "0.5";
+// })
+
+// Category.addEventListener("mouseleave", () => {
+//     showCategory.style.display = 'none';
+//     Browse.style.opacity = "1";
+
+// })
+
+// showCategory.addEventListener("mouseenter", () => {
+//     showCategory.style.display = 'block';
+//     showCategory.style.animation = 'showUp 0.5s 1';
+//     Browse.style.opacity = "0.5";
+// })
+
+
+
+
+
+
+
+
